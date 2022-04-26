117835162a53a4a2b44b0c046d237307137403ff
InfyQL <johngood1993@gmail.com>
2022-04-26 18:15:55 -0400

    Data cleaned and simualted

---
+++ scripts/00-simulation.R
@@ -0,0 +1,51 @@
+#### Preamble ####]
+# Purpose: Simulate dataset
+# Author: Jonathan Goodwin
+# Data: 26 April 2022
+# Contact: jon.goodwin@mail.utoronto.ca
+
+library(tidyverse)
+Set.seed(1)
+
+### Simulating dataset ###
+raw_data <- readr::read_csv("inputs/data/raw_data.csv"
+)
+x <- unique(cleaned_physical_activity$LocationDesc)
+y <- rep(x, each = 10)
+years <- c("2010","2011","2012","2013","2014","2015","2016","2017","2018","2019")
+years <- rep(years, times = 53)
+### Choosing a beta distribution since the values are proportions
+### From the previews of the dataset it seemed the mean number of 
+### people who ride bikes was around 0.05, so I chose a alpha = 2, beta = 38
+### Data given did not give more then 1 digit in the proportion so rounding 
+### to 3 digits
+z <-100*round(rbeta(530, shape1 = 2, shape2 = 38), digits = 3)
+df <- cbind(years,y, z)
+### Simulating the physical activity survey ###
+Questions <- c("Percent of adults who achieve at least 
+               300 minutes a week of moderate-intensity 
+               aerobic physical activity or 150 minutes a
+               week of vigorous-intensity aerobic activity
+               (or an equivalent combination)",
+                 "Percent of adults who engage in muscle-strengthening
+               activities on 2 or more days a week",
+                 "Percent of adults who achieve at least 150 minutes a
+               week of moderate-intensity aerobic physical activity or 
+               75 minutes a week of vigorous-intensity aerobic activity 
+               (or an equivalent combination)","Percent of adults who engage
+               in no leisure-time physical activity","Percent of adults aged
+               18 years and older who have an overweight classification",
+               "Percent of adults aged 18 years and older who have obesity")
+n <- rep(x, each = 36)
+n<- rep(n, times = 10)
+Question <- rep(Questions, each = 6)
+Question <- rep(Question, times = 53)
+Question <- rep(Question, times = 10)
+Age_years <- c("25 - 34", "55-64", "18-24","45-54", "35-44", "65 or older")
+Age_years <- rep(Age_years, times = 3180)
+years1 <- c("2011","2012","2013","2014","2015","2016","2017","2018","2019", "2020")
+years1<- rep(years1, each = 1908)
+### Had no indication of the average for each question so sampling from
+### a uniform distribution
+Value <-100*round(runif(19080, min = 0, max = 1))
+df2 <- cbind(years1,Question,Value,Age_years)
--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,36 +1,34 @@
-#### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
-# License: MIT
-# Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
+#### Preamble ####]
+# Purpose: To clean data from inputs
+# Author: Jonathan Goodwin
+# Data: 20 April 2022
+# Contact: jon.goodwin@mail.utoronto.ca
 
-
-#### Workspace setup ####
-# Use R Projects, not setwd().
+library(pointblank)
 library(haven)
 library(tidyverse)
 # Read in the raw data. 
 raw_data <- readr::read_csv("inputs/data/raw_data.csv"
                      )
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
+raw_data1 <- readr::read_csv("inputs/data/CDC_community survey.csv")
 
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
-         
+### Removing non relevant questions ###
+categories <- unique(raw_data$Question)
+df <- subset(raw_data, Question != "Percent of adults who report consuming vegetables less than one time daily")
+df <- subset(df, Question != "Percent of adults who report consuming fruit less than one time daily" )
+df <- subset(df, Question != "Percent of adults who achieve at least 150 minutes a week of moderate-intensity aerobic physical activity or 75 minutes a week of vigorous-intensity aerobic physical activity and engage in muscle-strengthening activities on 2 or more days a week")
+Columns <- c("YearEnd","LocationDesc", "Question", "Data_Value")
+raw_data1[Columns]
+df1 <- raw_data1[Columns]
+Columns1 <- c("YearEnd","LocationDesc", "Question", "Data_Value", "Age(years)")
+df2 <- df[Columns1]
+### Removing NA values, these values corresponded to stratification we
+### were not interested in or for insufficient sample size.
+df2 <- na.omit(df2)
+df2 <- subset(df2, LocationDesc != "Guam")
+df2 <- subset(df2, LocationDesc != "Virgin Islands")
 
-#### What's next? ####
 
-
-
-         
+### Write csv ###
+write.csv(x=df1, file = "cleaned_walk_or_bike.csv")
+write.csv(x=df2, file = "cleaned_physical_activity.csv")
